a0d98e5e4ca26a7c288061a8898dcc137bab37c8
gabrielaraujoz <garaujo.eng@gmail.com>
2020-10-19 20:32:30 -0300

    Aula Recycler View Pt 1

---
+++ Android/Aula22_RecyclerView/app/src/main/java/com/gabrielaraujoz/aula22_recyclerview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.gabrielaraujoz.aula22_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.minhaLista)
+        val viewAdapter = MeuAdapter(arrayListOf("Laranja", "Morango", "Sorvete", "Peixe"))
+
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+    }
+}
