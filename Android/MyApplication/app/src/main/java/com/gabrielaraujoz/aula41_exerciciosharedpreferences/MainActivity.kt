4327a5cb5616338b884c68ca43b14cc409c14b28
gabrielaraujoz <garaujo.eng@gmail.com>
2020-12-07 20:01:48 -0300

    Primeiro commit

---
+++ Android/MyApplication/app/src/main/java/com/gabrielaraujoz/aula41_exerciciosharedpreferences/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gabrielaraujoz.aula41_exerciciosharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
