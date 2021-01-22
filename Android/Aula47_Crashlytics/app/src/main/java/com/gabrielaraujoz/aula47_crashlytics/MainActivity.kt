91252d450b2c7284e0e9ff1a5a9dfc32c17526b0
gabrielaraujoz <garaujo.eng@gmail.com>
2021-01-22 19:54:01 -0300

    aula 47

---
+++ Android/Aula47_Crashlytics/app/src/main/java/com/gabrielaraujoz/aula47_crashlytics/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.gabrielaraujoz.aula47_crashlytics
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Creates a button that mimics a crash when clicked
+        val crashButton = Button(this)
+        crashButton.text = "Crash!"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+}
